f448c6fec8e98b158ca1228f29ecd742d0912507
Oskar Lyrstrand <oskar.lyrstrand@gmail.com>
2023-12-02 14:47:26 +0100

    Ignore http errors

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: {
+			handleHttpError: "ignore"
+		},
 		adapter: adapter({
 			fallback: '404.html'
 		}),
