555f25a1e0b3cfa72c96c6d8a211e7fe4c5081bd
Oskar Lyrstrand <oskar.lyrstrand@gmail.com>
2023-12-02 15:21:35 +0100

    Try vercel instead of GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		prerender: {
-			handleHttpError: "ignore"
-		},
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+		adapter: adapter(),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
