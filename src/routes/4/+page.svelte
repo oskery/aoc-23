dea2d9ce9c79d2048a867367ff06b107def4c1d9
Oskar Lyrstrand <oskar.lyrstrand@gmail.com>
2023-12-05 17:30:22 +0100

    Day 4 part 2

--- src/routes/4/+page.svelte
+++ src/routes/4/+page.svelte
@@ -2,7 +2,6 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	// data.input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`;
 	// Part 1
 	let sum = 0;
 
@@ -23,6 +22,38 @@
 
 		if (Array.from(winners).length) sum += Math.pow(2, Array.from(winners).length - 1);
 	});
+
+	// Part 2
+	let cards: Record<number, number[]> = {}; // for part 2
+
+	lines.reverse().forEach((line: string, i: number) => {
+		const card = lines.length - i;
+		let winners = new Set();
+
+		const [left, right] = line.split(': ')?.[1].split(' | ');
+		const winning = left
+			.trim()
+			.split(' ')
+			.map(Number)
+			.filter((x) => !!x);
+		const numbers = right?.trim().split(' ').map(Number);
+
+		winning.forEach((num: number) => (numbers?.includes(num) ? winners.add(num) : null));
+
+		if (Array.from(winners).length) {
+			cards[card] = Array.from(winners).map((_, i) => card + 1 + i);
+
+			cards[card] = cards[card].concat(
+				cards[card].map((c) => (c !== card ? cards[c] ?? [] : [])).flat()
+			);
+		}
+	});
+	let sum2 =
+		Object.keys(lines).length + Object.values(cards).reduce((acc, arr) => acc + arr.length, 0);
 </script>
 
 Part 1: {sum}
+
+<br />
+Part 2: {sum2}
+<br />
